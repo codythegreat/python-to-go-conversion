36d5d29b74db62afb05712e609d6b0c73b4514ff
codythegreat <maxiecody@gmail.com>
2019-02-13 19:50:09 +0000

    added cellular allocation program

---
+++ cellalloc.go
@@ -0,0 +1,50 @@
+// attempt to recreate account_entry_checker_V2 (from python) to go
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/ledongthuc/pdf"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	pdfText, err := readPdf("employees.pdf")
+	if err != nil {
+		panic(err)
+	}
+	cellBook, err := excelize.OpenFile("./cellular.xlsx")
+	if err != nil {
+		fmt.Printf("While opening cellular file: %v", err)
+	}
+
+	for i := 2; i != 3000; i++ {
+		name := strings.Split(cellBook.GetCellValue("sheet1", "M"+strconv.Itoa(i)), " ")
+		nameFormattedToPDF := name[1] + ", " + name[0]
+		nameWithInitial := name[1] + ", " + string(name[0][0])
+		if strings.Contains(pdfText, nameFormattedToPDF) {
+			cellBook.SetCellValue("Sheet1", "M"+strconv.Itoa(i), cellBook.GetCellValue("sheet1", "M"+strconv.Itoa(i))+" $")
+		} else if strings.Contains(pdfText, nameWithInitial) {
+			cellBook.SetCellValue("Sheet1", "M"+strconv.Itoa(i), cellBook.GetCellValue("sheet1", "M"+strconv.Itoa(i))+" $$")
+		} else {
+			cellBook.SetCellValue("Sheet1", "M"+strconv.Itoa(i), cellBook.GetCellValue("sheet1", "M"+strconv.Itoa(i))+" @")
+		}
+	}
+}
+func readPdf(path string) (string, error) {
+	f, r, err := pdf.Open(path)
+	// remember to close the pdf file
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	b, err := r.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+	buf.ReadFrom(b)
+	return buf.String(), nil
+}
